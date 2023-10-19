5741d4ca021afc74c63e4b094de9d6d2762cdfd1
KIM602 <kyk940608@gmail.com>
2023-10-19 15:54:06 +0900

    Initial commit

---
+++ lib/screen/main/tab/tab_item.dart
@@ -0,0 +1,28 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/favorite/f_favorite.dart';
+import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:flutter/material.dart';
+
+enum TabItem {
+  todo(Icons.event_note_outlined, 'Todo', HomeFragment()),
+  search(Icons.search, 'Search', FavoriteFragment(isShowBackButton: false));
+
+  final IconData activeIcon;
+  final IconData inActiveIcon;
+  final String tabName;
+  final Widget firstPage;
+
+  const TabItem(this.activeIcon, this.tabName, this.firstPage, {IconData? inActiveIcon})
+      : inActiveIcon = inActiveIcon ?? activeIcon;
+
+  BottomNavigationBarItem toNavigationBarItem(BuildContext context, {required bool isActivated}) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          key: ValueKey(tabName),
+          isActivated ? activeIcon : inActiveIcon,
+          color:
+              isActivated ? context.appColors.iconButton : context.appColors.iconButtonInactivate,
+        ),
+        label: tabName);
+  }
+}
