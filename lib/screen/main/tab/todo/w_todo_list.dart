9d1025f9199bd68a10a724092f9ab6171af1a8ae
KIM602 <kyk940608@gmail.com>
2023-10-20 17:36:41 +0900

    GetX로 변환하기

--- lib/screen/main/tab/todo/w_todo_list.dart
+++ lib/screen/main/tab/todo/w_todo_list.dart
@@ -1,19 +1,19 @@
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/screen/main/tab/todo/w_todo_item.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: context.holder.notifier,
-      builder: (context, todoList, child) {
-        return todoList.isEmpty
+    return GetBuilder<TodoDataHolder>(
+      builder: (todoData) {
+        return todoData.todoList.isEmpty
             ? '할일을 작성해보세요.'.text.size(30).makeCentered()
             : Column(
-                children: todoList.map((e) => TodoItem(e)).toList(),
+                children: todoData.todoList.map((e) => TodoItem(e)).toList(),
               );
       },
     );
